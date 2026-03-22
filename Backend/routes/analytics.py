16df1ea6f47d3982436ae74c5d084eadc0917229
Atharv Batule <atharvbatule9@.com>
2026-03-22 11:05:06 +0530

    update controllers and routes

---
+++ Backend/routes/analytics.py
@@ -0,0 +1,14 @@
+from flask import Blueprint,Flask, app, jsonify
+import pandas as pd
+from predict import predict_today
+from weatherforecast import fetch_weather_dataset
+from analytics import analytics
+
+
+analytics_bp = Blueprint("analytics", __name__)
+
+@analytics_bp.route("/analytics", methods=["GET"])
+def get_analytics():
+    lat = 15.2833
+    lon = 73.9833
+    return analytics(lat, lon)
--- Backend/routes/solar_forecast.py
+++ Backend/routes/solar_forecast.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify;
+from flask import Blueprint, Flask, jsonify;
 from solar_forecast import get_solar_forecast;
 from predict import predict_today;
 from weatherforecast import fetch_weather_dataset;
 
-app = Flask(__name__)
 
-@app.route("/solar-forecast", methods=["GET"])
+solar_bp = Blueprint("solar", __name__)
+@solar_bp.route("/solar-forecast", methods=["GET"])
 def solar_forecast():
     return get_solar_forecast(15.2833, 73.9833);
     
